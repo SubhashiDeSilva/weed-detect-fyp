12cf5657ae018f788c51fe751d53842209c48ba2
Subhashini De Silva <sudharmasubhashini@gmail.com>
2024-05-11 23:57:07 +0530

    Publish Repo - 0.0.2v
    I created about-us page and how-it-work page.

---
+++ web/client/src/app/about-us/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Page(){
+
+    return(
+        <main>
+            <div className={"container-fluid"}>
+                <div className={'row'}>
+                    <h1>About US</h1>
+                </div>
+            </div>
+        </main>
+    )
+}
