cd214645a716e51cb1c2846106c2309fa44362b1
Geethanimal <142132766+SubhashiDeSilva@users.noreply.github.com>
2024-05-13 04:32:09 +0530

    Publish Repo - 0.0.4v
    About US Page content Updated.

--- web/client/src/app/about-us/page.tsx
+++ web/client/src/app/about-us/page.tsx
@@ -1,11 +1,35 @@
 
-export default function Page(){
+export default function Page() {
 
-    return(
+    return (
         <main>
             <div className={"container-fluid"}>
                 <div className={'row'}>
-                    <h1>About US</h1>
+                    <div className={'col-6 mt-5 pt-5'}>
+                        <p className={'display-6 fw-lighter'}><a href={'/try-weed-detector'} className={'text-decoration-none'}></a> About Us</p>
+                        <p>Welcome to Weed Detect, your premier destination for cutting-edge weed detection technology. At Weed Detect, we are committed to revolutionizing the way weeds are identified and managed in agricultural and landscaping settings.
+                            <br/>
+                            <br/>
+With our state-of-the-art algorithms and image recognition technology, Weed Detect offers a comprehensive solution for identifying common weed species such as Echinochloa colona, Echinochloa crus-galli, Cyperus rotundus, Monochoria vaginalis, and Ludwigia perennis. Our platform harnesses the power of machine learning to provide accurate and efficient weed detection, helping farmers, gardeners, and land managers save time and resources.
+<br/>
+<br/>
+What sets Weed Detect apart is our unwavering dedication to accuracy. Our advanced algorithms have been rigorously tested and fine-tuned to achieve an impressive accuracy rate of 94%. This means you can trust Weed Detect to deliver reliable results, enabling you to make informed decisions about weed control and management strategies.
+<br/>
+<br/>
+At Weed Detect, we understand the challenges faced by those tasked with weed management. Whether you're a small-scale farmer, a landscaping professional, or a large agricultural enterprise, our goal is to empower you with the tools you need to effectively combat weeds and protect your crops and landscapes.
+<br/>
+<br/>
+But Weed Detect is more than just a tool – it's a commitment to innovation and sustainability. By accurately identifying weeds, we help reduce the reliance on chemical herbicides, promote eco-friendly farming practices, and support the preservation of natural ecosystems.
+<br/>
+<br/>
+Join the growing community of users who rely on Weed Detect to streamline their weed management processes and achieve greater efficiency and productivity. Experience the power of precision weed detection with Weed Detect – because when it comes to weed management, accuracy matters.
+<br/>
+<br/>
+Discover the future of weed detection with Weed Detect. Try it today and see the difference for yourself!</p>
+                    </div>
+                    <div className={'col-6'}>
+                        <img className={'img-fluid vh-100 p-2 w-100'} src={'/images/Rectangle 3.png'} title={''} />
+                    </div>
                 </div>
             </div>
         </main>
